fe27f11b17a28601aed1ea45aab8068ff7a32f8c
Alina <alishad29@mail.ru>
2022-11-19 00:48:00 +0300

    odd

--- odd_nmbrs.cs
+++ odd_nmbrs.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = {7, 4, 6, 9, 10, 12, 18};
+int n = array.Length;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index]%2==0)
+    {
+        Console.WriteLine(array[index]);
+        index ++;
+    }
+    else
+    {
+        index ++;
+    }
+}
